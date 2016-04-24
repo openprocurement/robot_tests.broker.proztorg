09c98f3222bd3e640ad0e219656fc7292b9131a5
selurvedu <selurvedu@users.noreply.github.com>
2016-04-24 15:32:58 +0000

    Include essential files when generating a package
    This change will include python modules and *.robot files
    when building a package using setup.py.

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ broker_name = 'proztorg'
 pkg_name = 'robot_tests.broker.{}'.format(broker_name)
 
 setup(name=pkg_name,
-    version='0.0.dev1',
-    description='{} broker for OpenProcurement Robot tests'.format(broker_name),
-    author='',
-    author_email='',
-    url='https://github.com/openprocurement/{}'.format(pkg_name),
-)
+      version='0.0.dev1',
+      description='{} broker'
+      ' for OpenProcurement Robot tests'.format(broker_name),
+      author='',
+      author_email='',
+      url='https://github.com/openprocurement/{}'.format(pkg_name),
+      packages=[pkg_name],
+      package_dir={pkg_name: '.'},
+      package_data={pkg_name: ['*.robot']}
+      )
